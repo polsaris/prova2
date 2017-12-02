bf63fc5e7d2322064a63840f513f379fd09a019e
polsaris <34107486+polsaris@users.noreply.github.com>
2017-12-02 11:38:07 +0100

    Add files via upload

---
+++ analogclock.h
@@ -0,0 +1,32 @@
+#ifndef ANALOGCLOCK_H
+#define ANALOGCLOCK_H
+
+#include <QWidget>
+#include <QTimer>
+#include <QTime>
+
+namespace Ui {
+class AnalogClock;
+}
+
+class AnalogClock : public QWidget
+{
+    Q_OBJECT
+    
+public:
+    explicit AnalogClock(QWidget *parent = 0);
+    ~AnalogClock();
+    QTimer *timer;
+    QTime time;
+protected:
+    void paintEvent(QPaintEvent *event);
+
+private slots:
+    void on_toolButton_clicked();
+
+private:
+    Ui::AnalogClock *ui;
+
+};
+
+#endif // ANALOGCLOCK_H
